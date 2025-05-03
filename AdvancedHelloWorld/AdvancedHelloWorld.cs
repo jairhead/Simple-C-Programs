efd6afb8b1a90366c833956a24d6c2e3fcac6017
Jared McKneely <jared.mckneely@gmail.com>
2025-05-03 12:59:07 -0600

    Added AdvancedHelloWorld.cs

---
+++ AdvancedHelloWorld/AdvancedHelloWorld.cs
@@ -0,0 +1,16 @@
+// AdvancedHelloWorld.cs
+using System;
+
+namespace SimplePrograms {
+  class AdvancedHelloWorld {
+    // Main
+    static void Main(string[] args) {
+      ParseArgs(args);
+    }
+
+    // ParseArgs Helper Method
+    private void ParseArgs(string[] args) {
+        System.Console.WriteLine("AdvancedHelloWorld::ParseArgs()");
+    }
+  }
+}
